1054c46b7a5f3a20efd200b3d965b663e128d4e5
Júlia Rodrigues <heylavenderbee@gmail.com>
2026-02-22 20:09:18 -0300

    feat - palavras arquivo csv
    Agora, as palavras são importadas de um arquivo CSV, sendo mais fácil para gerencia-las. Assim como adição de um Frame, para organizar as tentativas

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from tkinter import *
 import random
+import pandas
+import numpy
 
 app = Tk()
 app.title("Termo")
 app.geometry("500x500")
 app.configure(background="#dde")
 chute = ""
+entry_str = StringVar()
 response = StringVar()
 letra1 = StringVar()
 letra2 = StringVar()
@@ -13,26 +16,47 @@ letra3 = StringVar()
 letra4 = StringVar()
 letra5 = StringVar()
 
-palavras = ["PARDO","BARCO","VERDE","ANZOL","LABIO","PUNIR","CASAL","PULSO","FALSO","PILHA","CACHO","ZEBRA","FERRO","PRETO","PEITO","UNIAO","CURVA","SUBIR","CORDA","BRACO","SAMBA","NATAL","CARRO","MEDIA","MEDIR","BOMBA"]
-palavra_escolhida = palavras[random.randint(0, len(palavras)-1)]
+palavras = []
+palavra_escolhida = ""
 
 Label(app, text="Bem-vindo ao Termo em Python!",background = "#dde",foreground="#009",anchor=W).place(x=10, y=10,width=200,height=20)
-entry = Entry(app)
+entry = Entry(app, textvariable=entry_str)
 entry.place(x=10,y=40,width=200,height=40)
-button = Button(app,text="Me clique!")
+button = Button(app,text="Adicionar palavra")
 button.place(x=10,y=90)
 result_label = Label(app, text="a",background = "#dde",textvariable=response).place(x=10,y=120)
 
-label1 = Label(app, background="gray",textvariable=letra1)
-label1.place(x=20,y=220,width=25,height=25,anchor="center")
-label2 = Label(app, background="gray",textvariable=letra2)
-label2.place(x=50,y=220,width=25,height=25,anchor="center")
-label3 = Label(app, background="gray",textvariable=letra3)
-label3.place(x=80,y=220,width=25,height=25,anchor="center")
-label4 = Label(app, background="gray",textvariable=letra4)
-label4.place(x=110,y=220,width=25,height=25,anchor="center")
-label5 = Label(app, background="gray",textvariable=letra5)
-label5.place(x=140,y=220,width=25,height=25,anchor="center")
+main_frame = Frame(app, padx=30,pady=30,bg="#dde")
+main_frame.place(x=20,y=150,width=190,height=300)
+
+label1 = Label(main_frame, background="gray",textvariable=letra1)
+label1.place(width=25,height=25,anchor="center")
+label2 = Label(main_frame, background="gray",textvariable=letra2)
+label2.place(x=30,width=25,height=25,anchor="center")
+label3 = Label(main_frame, background="gray",textvariable=letra3)
+label3.place(x=60,width=25,height=25,anchor="center")
+label4 = Label(main_frame, background="gray",textvariable=letra4)
+label4.place(x=90,width=25,height=25,anchor="center")
+label5 = Label(main_frame, background="gray",textvariable=letra5)
+label5.place(x=120,width=25,height=25,anchor="center")
+
+def read_words_from_csv():
+    df = pandas.read_csv("words.csv", usecols=["PALAVRA"])
+    data_array = df.to_numpy()
+    print(data_array)
+    i = 0
+    for data in data_array:
+        data = str(data).replace("['","")
+        data = data.replace("']","")
+        data = data.upper()
+        palavras.append(data)
+        i += 1
+    global palavra_escolhida
+    print(palavras)
+    palavra_escolhida = palavras[random.randint(0, len(palavras)-1)]
+    print("Entre um total de: ",i,"itens, a palavra é: ",palavra_escolhida)
+    
+read_words_from_csv()
 
 def change_label_bgcolor_green(n):
     n += 1
@@ -80,10 +104,10 @@ def checar_chute():
     
     #primeiro verifica se o chute tem 5 letras
     if len(chute) != 5:
-        print("O chute deve conter 5 letras.")
         response.set("O chute deve conter 5 letras")
         return
     
+    entry_str.set("")
     letra1.set(chute[0])
     letra2.set(chute[1])
     letra3.set(chute[2])
@@ -91,7 +115,6 @@ def checar_chute():
     letra5.set(chute[4])
     
     if chute == palavra_escolhida:
-        print(f"Parabéns, você acertou! A palavra era: {palavra_escolhida}.")
         response.set(f"Parabéns, você acertou! A palavra era: {palavra_escolhida}.")
         change_label_bgcolor_green(0)
         change_label_bgcolor_green(1)
@@ -102,19 +125,17 @@ def checar_chute():
         i = 0
         for letra in chute:
             if letra in palavra_escolhida:
-                print(f"A letra {letra} está na palavra.")
                 change_label_bgcolor_yellow(i)
                 if palavra_escolhida[i] == letra:
                     change_label_bgcolor_green(i)
             else:
-                print(f"A letra {letra} não está na palavra.")
                 change_label_bgcolor_gray(i)
             i += 1
-        print("Continue tentando.")
         response.set("Continue tentando.")
         chute = ""
 
 button.config(command=checar_chute)
 app.mainloop()
+
 
 
